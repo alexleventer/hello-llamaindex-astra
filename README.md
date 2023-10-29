b6900359b46cde64585d8ebd9afbe6875e0be998
Stefano Lottini <hemidactylus@users.noreply.github.com>
2023-10-29 22:07:39 +0100

    Sl fixes and upgrade llama (#1)
    * upgrade to modern cassio+llama; fix requirements.txt; code style; standard env var naming
    
    * remove unused import (openai)

--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 A simple app demonstrating the Llama Index/Astra Integration.
 
 ## Getting Started
-1. Create a .env file based on the .env example
-2. Run the app!
+
+1. Create a `.env` file based on the `.env` example (see below)
+2. Install the dependencies with e.g. `pip install -r requirements.txt`. _(Python 3.8+ required. Working in a virtual environment is suggested as general best practice)_.
+3. Run the app with `python app.py` !
+
+### The dot-env file
+
+The environment variables expected in the `.env` file are:
+
+- `OPENAI_API_KEY`, your OpenAI API key.
+- `ASTRA_DB_APPLICATION_TOKEN`, the Token to access your Astra DB instance. The token must be created with role "Database Administrator".
+- `ASTRA_DB_ID`, the ID of your Astra DB, which you can find at the top of your dashboard in the Astra UI.
+= `ASTRA_DB_KEYSPACE` **(optional)**, the name of your keyspace in the database. If omitted, the app will default to using the main keyspace originally created with the DB.
--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 import os
-
-import openai
 from pathlib import Path
+
+import cassio
 from dotenv import load_dotenv
-from llama_index import (VectorStoreIndex, StorageContext, )
+from llama_index import StorageContext, VectorStoreIndex
 from llama_index.vector_stores import CassandraVectorStore
 from llama_index import download_loader
-from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
+
 
 load_dotenv()
 
-ASTRA_DB_TOKEN = os.getenv("ASTRA_DB_TOKEN")
-ASTRA_DB_SCB = os.getenv("ASTRA_DB_SCB")
-ASTRA_KEYSPACE = os.getenv("ASTRA_KEYSPACE")
-openai.api_key = os.getenv("OPENAI_API_KEY")
+ASTRA_DB_ID = os.environ["ASTRA_DB_ID"]
+ASTRA_DB_APPLICATION_TOKEN = os.environ["ASTRA_DB_APPLICATION_TOKEN"]
+ASTRA_DB_KEYSPACE = os.getenv("ASTRA_DB_KEYSPACE")
 
-cluster = Cluster(cloud={"secure_connect_bundle": ASTRA_DB_SCB},
-                  auth_provider=PlainTextAuthProvider("token", ASTRA_DB_TOKEN, ), )
-session = cluster.connect()
+cassio.init(
+    database_id=ASTRA_DB_ID,
+    token=ASTRA_DB_APPLICATION_TOKEN,
+    keyspace=ASTRA_DB_KEYSPACE,
+)
 
-cassandra_store = CassandraVectorStore(session=session, keyspace=ASTRA_KEYSPACE, table="nasa",
-    embedding_dimension=1536, )
+cassandra_store = CassandraVectorStore(table="nasa", embedding_dimension=1536)
 
 PDFReader = download_loader("PDFReader")
 
@@ -29,7 +28,10 @@ loader = PDFReader()
 documents = loader.load_data(file=Path('./documents/nasa-rockets-guide.pdf'))
 
 storage_context = StorageContext.from_defaults(vector_store=cassandra_store)
-index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+index = VectorStoreIndex.from_documents(
+    documents,
+    storage_context=storage_context,
+)
 
 
 def query_store(question):
@@ -38,5 +40,5 @@ def query_store(question):
 
 
 if __name__ == '__main__':
-    response = query_store('What sort of experiments did Galileo conduct?')
-    print(response)
+    answer = query_store('What sort of experiments did Galileo conduct?')
+    print(answer.response)
